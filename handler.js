5440a06c31c5c0e229882d7dcf17085164ebb555
Javier Orlando Mantilla Portilla <jomantil@bancolombia.com.co>
2024-12-18 10:10:06 -0500

    Se actualiza creacion de cola y funcion para enviar orden

--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
 
 const { v4: uuidv4 } = require('uuid');
+const { SQSClient, SendMessageCommand } = require ("@aws-sdk/client-sqs");
+// Create an SQS client
+const sqsClient = new SQSClient({ region: process.env.REGION });
 
 exports.newOrder = async (event) => {
     const orderID= uuidv4();
@@ -20,6 +23,9 @@ exports.newOrder = async (event) => {
     }   
     console.log(orderDetails)
     const order = { orderID, ...orderDetails}; 
+    
+    const PENDING_ORDERS_QUEUE_URL = process.env.PENDING_ORDERS_QUEUE;
+    await sendMessageToSQS(order,PENDING_ORDERS_QUEUE_URL);
     return {
         statusCode: 200,
         body: JSON.stringify(
@@ -55,3 +61,38 @@ exports.getOrder = async (event) => {
   
     return;
   };
+
+  exports.sendOrder = async (event) => {
+    console.log(event);
+  
+    const order = {
+      orderId: event.orderId,
+      pizza: event.pizza,
+      customerId: event.pizza
+    }
+  
+    const ORDERS_TO_SEND_QUEUE_URL = process.env.ORDERS_TO_SEND_QUEUE
+  
+    await sendMessageToSQS(order, ORDERS_TO_SEND_QUEUE_URL);
+  
+    return;
+  }
+
+
+  async function sendMessageToSQS(message,queueURL) {
+
+    const params = {
+      QueueUrl: queueURL,
+      MessageBody: JSON.stringify(message)
+    };  
+    console.log(params);  
+    try {
+      const command = new SendMessageCommand(params);
+      const data = await sqsClient.send(command);
+      console.log("Message sent successfully:", data.MessageId);
+      return data;
+    } catch (error) {
+      console.error("Error sending message:", error);
+      throw error;
+    }
+  }
