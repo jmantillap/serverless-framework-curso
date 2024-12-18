48cf2841d5df74e0b6564fa0220e7536e51da9df
Javier Orlando Mantilla Portilla <jomantil@bancolombia.com.co>
2024-12-18 18:06:27 -0500

    Se modifican archivos

--- handler.js
+++ handler.js
@@ -1,16 +1,23 @@
 
 const { v4: uuidv4 } = require('uuid');
-const { SQSClient, SendMessageCommand } = require ("@aws-sdk/client-sqs");
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
 // Create an SQS client
 const sqsClient = new SQSClient({ region: process.env.REGION });
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand, UpdateCommand, GetCommand } = require("@aws-sdk/lib-dynamodb");
+
+// Create a DynamoDB client
+const client = new DynamoDBClient({ region: process.env.REGION });
+// Create a DynamoDB document client
+const docClient = DynamoDBDocumentClient.from(client);
 
 exports.newOrder = async (event) => {
-    const orderID= uuidv4();
-    console.log(orderID)
+    const orderId = uuidv4();
+    console.log(orderId)
     let orderDetails;
     try {
         orderDetails = JSON.parse(event.body);
-    }catch (e) {
+    } catch (e) {
         console.log('Error parsing order details');
         console.log(e);
         return {
@@ -20,12 +27,14 @@ exports.newOrder = async (event) => {
                     message: 'Error parsing order details',
                 }),
         };
-    }   
+    }
     console.log(orderDetails)
-    const order = { orderID, ...orderDetails}; 
-    
+    const order = { orderId, ...orderDetails };
+    // Save order in the database
+    await saveItemToDynamoDB(order);
+
     const PENDING_ORDERS_QUEUE_URL = process.env.PENDING_ORDERS_QUEUE;
-    await sendMessageToSQS(order,PENDING_ORDERS_QUEUE_URL);
+    await sendMessageToSQS(order, PENDING_ORDERS_QUEUE_URL);
     return {
         statusCode: 200,
         body: JSON.stringify(
@@ -37,62 +46,143 @@ exports.newOrder = async (event) => {
 
 exports.getOrder = async (event) => {
     console.log(event);
-  
     const orderId = event.pathParameters.orderId
-  
-    const orderDetails = {
-      "pizza": "Margarita",
-      "customerId": 1,
-      "order_status": "COMPLETED"
+
+    try {
+        const order = await getItemFromDynamoDB(orderId);
+        console.log(order)
+        return {
+            statusCode: 200,
+            body: JSON.stringify(order)
+        };
+    } catch (error) {
+        console.error("Error retrieving order:", error);
+
+        if (error.name === "ItemNotFoundException") {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: "Order not found" }),
+            };
+        } else {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: "Error retrieving order" }),
+            };
+        }
     }
-  
-    const order = {orderId, ...orderDetails}
-  
-    console.log(order);
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({message: order})
-    };
-  }
+}
 
-  exports.prepOrder = async (event) => {
+exports.prepOrder = async (event) => {
     console.log(event);
-  
+    const body = JSON.parse(event.Records[0].body);
+    const orderId = body.orderId;
+    await updateStatusInOrder(orderId, "COMPLETED");
     return;
-  };
+};
 
-  exports.sendOrder = async (event) => {
+exports.sendOrder = async (event) => {
     console.log(event);
-  
+
     const order = {
-      orderId: event.orderId,
-      pizza: event.pizza,
-      customerId: event.pizza
+        orderId: event.orderId,
+        pizza: event.pizza,
+        customerId: event.pizza
     }
-  
+
     const ORDERS_TO_SEND_QUEUE_URL = process.env.ORDERS_TO_SEND_QUEUE
-  
+
     await sendMessageToSQS(order, ORDERS_TO_SEND_QUEUE_URL);
-  
+
     return;
-  }
+}
 
 
-  async function sendMessageToSQS(message,queueURL) {
+async function sendMessageToSQS(message, queueURL) {
 
     const params = {
-      QueueUrl: queueURL,
-      MessageBody: JSON.stringify(message)
-    };  
-    console.log(params);  
+        QueueUrl: queueURL,
+        MessageBody: JSON.stringify(message)
+    };
+    console.log(params);
     try {
-      const command = new SendMessageCommand(params);
-      const data = await sqsClient.send(command);
-      console.log("Message sent successfully:", data.MessageId);
-      return data;
+        const command = new SendMessageCommand(params);
+        const data = await sqsClient.send(command);
+        console.log("Message sent successfully:", data.MessageId);
+        return data;
     } catch (error) {
-      console.error("Error sending message:", error);
-      throw error;
+        console.error("Error sending message:", error);
+        throw error;
     }
-  }
+}
+
+async function saveItemToDynamoDB(item) {
+
+    const params = {
+        TableName: process.env.ORDERS_TABLE,
+        Item: item
+    };
+    console.log(params);
+    try {
+        const command = new PutCommand(params);
+        const response = await docClient.send(command);
+        console.log("Item saved successfully:", response);
+        return response;
+    } catch (error) {
+        console.error("Error saving item:", error);
+        throw error;
+    }
+}
+
+async function updateStatusInOrder(orderId, status) {
+
+    const params = {
+        TableName: process.env.ORDERS_TABLE,
+        Key: { orderId },
+        UpdateExpression: "SET order_status = :c",
+        ExpressionAttributeValues: {
+            ":c": status
+        },
+        ReturnValues: "ALL_NEW"
+    };
+
+    console.log(params);
+
+    try {
+        const command = new UpdateCommand(params);
+        const response = await docClient.send(command);
+        console.log("Item updated successfully:", response.Attributes);
+        return response.Attributes;
+    } catch (err) {
+        console.error("Error updating item:", err);
+        throw err;
+    }
+}
+
+async function getItemFromDynamoDB(orderId) {
+
+    const params = {
+        TableName: process.env.ORDERS_TABLE,
+        Key: { orderId },
+    };
+    console.log(params);
+    try {
+        const command = new GetCommand(params);
+        const response = await docClient.send(command);
+
+        if (response.Item) {
+            console.log("Item retrieved successfully:", response.Item);
+            return response.Item;
+        } else {
+            console.log("Item not found");
+
+            let notFoundError = new Error("Item not found");
+            notFoundError.name = "ItemNotFoundException";
+            throw notFoundError;
+        }
+
+    } catch (error) {
+        console.error("Error retrieving item:", error);
+        throw error;
+    }
+
+}
